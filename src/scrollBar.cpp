cdf06392e9d5f0f3c5b722fae281505ac4e13fbd
Nicolas Peri <nperi89@gmail.com>
2015-10-28 21:47:26 -0200

    j

---
+++ src/scrollBar.cpp
@@ -0,0 +1,196 @@
+//
+//  textInput.cpp
+//  NIMP_Prototipe
+//
+//  Created by Mili Garicoits on 10/3/15.
+//
+//
+
+#include "textInput.h"
+#include "textInputEvent.h"
+#include "ofxComposer.h"
+#include "ofxUISuperCanvas.h"
+
+textInput::textInput(string _name, string _textstring, float w, float h, float x, float y, int _size) : ofxUITextInput()
+{
+    init(_name, _textstring, w, h, x, y, _size);
+    
+    nodes.push_back("gif");
+    nodes.push_back("image");
+    nodes.push_back("shader");
+    nodes.push_back("texture");
+    nodes.push_back("video");
+    nodes.push_back("camera");
+}
+
+void textInput::setDropdownList(ofxUIDropDownList* dl) {
+    
+    dl->open();
+    dl->setVisible(false);
+    dl->setAutoClose(true);
+    
+    dl->addToggles(nodes);
+    dl->setToggleVisibility(false);
+    
+    this->addEmbeddedWidget(dl);
+    this->dropdownList = dl;
+    
+    ofAddListener(((ofxUISuperCanvas*) dl->getCanvasParent())->newGUIEvent,this,&textInput::guiEvent);
+}
+
+ofxUIDropDownList* textInput::getDropdownList() {
+    return this->dropdownList;
+}
+
+void textInput::keyPressed(int key) {
+    
+    ofxUITextInput::keyPressed(key);
+    if(clicked)
+    {
+        string input = this->getTextString();
+        for(auto n : this->dropdownList->getToggles()) {
+            if (n->getName().find(input) != -1)
+                n->setVisible(true);
+            else n->setVisible(false);
+        }
+
+        if (not this->dropdownList->isOpen()) this->dropdownList->open();
+    }
+}
+
+void textInput::mouseDragged(int x, int y, int button) {
+    
+    ofxUITextInput::mouseDragged(x, y, button);
+    
+    if ((this->draggable) and (this->hit)) {
+        this->dropdownList->getRect()->setX(x - hitPoint.x);
+        this->dropdownList->getRect()->setY(y - hitPoint.y);
+    }
+}
+
+void textInput::guiEvent(ofxUIEventArgs &e){
+    
+    if(e.widget == this->dropdownList){
+        this->setTextString(this->dropdownList->getSelected()[0]->getName());
+        
+        textInputEvent e;
+        e.point.set(this->getRect()->getX(), this->getRect()->getY());
+        e.widget = this;
+        e.type = this->dropdownList->getSelected()[0]->getName();
+        
+        ofFileDialogResult openFileResult;
+        
+        if (e.type == "camera") {
+            
+            e.type = "ofVideoGrabber";
+        }
+        else if (e.type == "image"){
+            
+            e.type = "ofImage";
+            
+            openFileResult = ofSystemLoadDialog("Select an image (.jpg, .jpeg, .png, .bmp or .gif)");
+            
+            if (openFileResult.bSuccess){
+                
+                ofFile file (openFileResult.getPath());
+                
+                if (file.exists()){
+                    
+                    string fileExtension = ofToUpper(file.getExtension());
+                    
+                    //We only want images
+                    if (fileExtension == "JPG"  ||
+                        fileExtension == "PNG"  ||
+                        fileExtension == "JPEG" ||
+                        fileExtension == "GIF"  ||
+                        fileExtension == "BMP"  ) {
+                        e.path = openFileResult.getPath();
+                    }
+                    else return;
+                }
+                file.close();
+            }
+            else return;
+        }
+        else if (e.type == "video") {
+            
+            e.type = "ofVideoPlayer";
+            
+            openFileResult = ofSystemLoadDialog("Select a video (.mov, .mpg, .mp4 or .m4v)");
+            
+            if (openFileResult.bSuccess){
+                
+                ofFile file (openFileResult.getPath());
+                
+                if (file.exists()){
+                    
+                    string fileExtension = ofToUpper(file.getExtension());
+                    
+                    //We only want videos
+                    if (fileExtension == "MOV" ||
+                        fileExtension == "MPG" ||
+                        fileExtension == "MP4" ||
+                        fileExtension == "M4V" ) {
+                        e.path = openFileResult.getPath();
+                    }
+                    else return;
+                }
+                file.close();
+            }
+            else return;
+        }
+        else if (e.type == "shader") {
+            
+            e.type = "ofShader";
+            
+            openFileResult = ofSystemLoadDialog("Select a shader (.fs or .frag)");
+            
+            if (openFileResult.bSuccess){
+                
+                ofFile file (openFileResult.getPath());
+                
+                if (file.exists()){
+                    
+                    string fileExtension = ofToUpper(file.getExtension());
+                    
+                    //We only want videos
+                    if (fileExtension == "FRAG" ||
+                        fileExtension == "FS"   ) {
+                        e.path = openFileResult.getPath();
+                    }
+                    else return;
+                }
+                file.close();
+            }
+            else return;
+        }
+        else if (e.type == "texture") {
+            
+            e.type = "ofTexture";
+            
+            openFileResult = ofSystemLoadDialog("Select a texture (.lut or .cube)");
+            
+            if (openFileResult.bSuccess){
+                
+                ofFile file (openFileResult.getPath());
+                
+                if (file.exists()){
+                    
+                    string fileExtension = ofToUpper(file.getExtension());
+                    
+                    //We only want videos
+                    if (fileExtension == "LUT"  ||
+                        fileExtension == "CUBE" ) {
+                        e.path = openFileResult.getPath();
+                    }
+                    else return;
+                }
+                file.close();
+            }
+            else return;
+        }
+        
+        ofNotifyEvent(createNode, e , this);
+        
+    }
+}
---
+++ src/scrollBar.h
@@ -0,0 +1,39 @@
+//
+//  textInput.h
+//  NIMP_Prototipe
+//
+//  Created by Mili Garicoits on 10/3/15.
+//
+//
+
+#ifndef __NIMP_Prototipe__textInput__
+#define __NIMP_Prototipe__textInput__
+
+#include <stdio.h>
+#include "ofxUITextInput.h"
+#include "ofxUIDropDownList.h"
+#include "ofxUIEventArgs.h"
+#include "textInputEvent.h"
+
+class textInput : public ofxUITextInput
+{
+    
+public:
+    
+    textInput(string _name, string _textstring, float w, float h = 0, float x = 0, float y = 0, int _size = OFX_UI_FONT_SMALL);
+    void setDropdownList(ofxUIDropDownList* dl);
+    ofxUIDropDownList* getDropdownList();
+    
+    void keyPressed(int key);
+    void mouseDragged(int x, int y, int button);
+    void guiEvent(ofxUIEventArgs &e);
+    
+    ofEvent<textInputEvent> createNode;
+
+protected:
+    
+    ofxUIDropDownList *dropdownList;
+    vector<string> nodes;
+};
+
+#endif /* defined(__NIMP_Prototipe__textInput__) */
