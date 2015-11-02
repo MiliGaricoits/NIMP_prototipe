2a6b566974365cfd4d3f6a382f1b18b87e27ffaf
MiliGaricoits <mili.garicoits@gmail.com>
2015-11-01 21:49:16 -0300

    ofxPatch extension

---
+++ src/patch.cpp
@@ -0,0 +1,11 @@
+//
+//  patch.cpp
+//  NIMP_Prototipe
+//
+//  Created by Mili Garicoits on 10/31/15.
+//
+//
+
+#include "patch.h"
+
+patch::patch() : ofxPatch() { }
---
+++ src/patch.h
@@ -0,0 +1,24 @@
+//
+//  patch.h
+//  NIMP_Prototipe
+//
+//  Created by Mili Garicoits on 10/31/15.
+//
+//
+
+#ifndef __NIMP_Prototipe__patch__
+#define __NIMP_Prototipe__patch__
+
+#include <stdio.h>
+#include "ofxPatch.h"
+#include "enumerations.h"
+
+class patch : public ofxPatch {
+    
+public:
+    
+    patch();
+    
+};
+
+#endif /* defined(__NIMP_Prototipe__patch__) */
